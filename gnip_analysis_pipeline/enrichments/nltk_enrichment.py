d2902eb249a5e4ae339f1b66d1f7748d0ce48287
Jeff Kolb <jeffakolb@gmail.com>
2016-07-20 21:54:45 +0000

    Add error handling in nltk enrichment

--- gnip_analysis_pipeline/enrichments/nltk_enrichment.py
+++ gnip_analysis_pipeline/enrichments/nltk_enrichment.py
@@ -27,7 +27,46 @@ class NLTKPOSBody(BaseEnrichment):
     def __repr__(self):
         return "Use the NLTK part-of-speech tagger on the Tweet body tokens."
 
+class NLTKTokenizeBio(BaseEnrichment):
+    def enrichment_value(self,tweet):
+        """ 
+        Account for the fact that:
+            - user bio can be None 
+            - user bio can be non-existent (key doesn't exist) 
+        """
+        try:
+            bio = tweet['actor']['summary']
+        except KeyError:
+            return []
+        if bio is not None:
+            return self.tokenize(bio)  
+        else:
+            return []
+    
+class NLTKSpaceTokenizeBio(NLTKTokenizeBio): 
+    def __init__(self):
+        self.tokenize = SpaceTokenizer().tokenize
+    def __repr__(self):
+        return "Use the NLTK SpaceTokenizer to parse the Tweet body."
+
+class NLTKWordTokenizeBio(NLTKTokenizeBio):
+    def __init__(self):
+        self.tokenize = word_tokenize
+    def __repr__(self):
+        return "Use the default NLTK word tokenizer to parse the Tweet body."
+
+class NLTKPOSBio(BaseEnrichment):
+    def __init__(self):
+        self.tagger = pos_tag
+    def enrichment_value(self,tweet):
+        return self.tagger( tweet['enrichments']['NLTKWordTokenizeBio'] )
+    def __repr__(self):
+        return "Use the NLTK part-of-speech tagger on the Tweet body tokens."
+
 class_list = ["NLTKSpaceTokenizeBody"
         , "NLTKWordTokenizeBody"
         , "NLTKPOSBody"
+        , 'NLTKSpaceTokenizeBio'
+        , 'NLTKWordTokenizeBio'
+        , 'NLTKPOSBio'
         ]
