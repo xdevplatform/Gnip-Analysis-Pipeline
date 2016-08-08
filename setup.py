f7abaf3e697c726d3863d08df55851155ee14e7e
jeffakolb <jeffakolb@gmail.com>
2016-08-08 11:20:50 -0600

    Add user_id_evaluator script

--- setup.py
+++ setup.py
@@ -4,7 +4,8 @@ setup(name='gnip_analysis_pipeline',
         packages=find_packages(),
         scripts=['tweet_enricher.py', 
             'tweet_time_series_builder.py', 
-            'tweet_evaluator.py'
+            'tweet_evaluator.py',
+            'user_id_evaluator.py'
             ],
         version='0.1',
         install_requires=['sngrams','matplotlib','pyyaml','requests','requests_oauthlib','pyfarmhash','nltk'] 
